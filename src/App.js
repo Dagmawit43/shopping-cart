ffe4c33881592227819f10b58cf67c9886e534b6
Dagmawit Gebreweld <dagmawitgebreweld43@gmail.com>
2025-04-30 08:17:20 +0000

    try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+
 import './App.css';
 import Nav from './Components/Nav';
-import Shop from './pages/shop/Shop';
-import Cart from './pages/cart/Cart';
+import Shop from './pages/shop/Shop'
+import Cart from './pages/cart/Cart' 
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import ShopContext from './context/shop-context';
 
 function App() {
   return (
     <div className="App">
       <ShopContext>
-        <Router>
-          <Nav />
-          <Routes>
-            {/* Redirect from root path to /shop */}
-            <Route path="/" element={<Navigate to="/" replace />} />
-            <Route path="/" element={<Shop />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </Router>
+      <Router>
+        <Nav/>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+      </Router>
       </ShopContext>
+      
+      
     </div>
   );
 }
